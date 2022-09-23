7e215ddc761a10595309eea65f41107719b794a0
Brazy <me@brazy.one>
2022-09-23 19:24:42 +0300

    Add items

---
+++ src/components/Cart.js
@@ -0,0 +1,9 @@
+function Cart() {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+}
+
+export default Cart;
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import s1 from './images/s1.jpg';
 import s2 from './images/s2.jpg';
 import s3 from './images/s3.jpg';
@@ -15,100 +16,130 @@ import s14 from './images/s14.jpg';
 import s15 from './images/s15.jpg';
 
 function Shop() {
-  const items = [
-    {
-      img: s1,
-      name: 'Sneakers',
-      price: '256',
-    },
-    {
-      img: s2,
-      name: 'Sneakers',
-      price: '728',
-    },
-    {
-      img: s3,
-      name: 'Sneakers',
-      price: '376',
-    },
-    {
-      img: s4,
-      name: 'Sneakers',
-      price: '643',
-    },
-    {
-      img: s5,
-      name: 'Sneakers',
-      price: '234',
-    },
-    {
-      img: s6,
-      name: 'Sneakers',
-      price: '865',
-    },
-    {
-      img: s7,
-      name: 'Sneakers',
-      price: '967',
-    },
-    {
-      img: s8,
-      name: 'Sneakers',
-      price: '120',
-    },
-    {
-      img: s9,
-      name: 'Sneakers',
-      price: '324',
-    },
-    {
-      img: s10,
-      name: 'Sneakers',
-      price: '460',
-    },
-    {
-      img: s11,
-      name: 'Sneakers',
-      price: '521',
-    },
-    {
-      img: s12,
-      name: 'Sneakers',
-      price: '743',
-    },
-    {
-      img: s13,
-      name: 'Sneakers',
-      price: '853',
-    },
-    {
-      img: s14,
-      name: 'Sneakers',
-      price: '634',
-    },
-    {
-      img: s15,
-      name: 'Sneakers',
-      price: '548',
-    },
-  ];
+  const [shop, setShop] = useState([]);
+
+  useEffect(() => {
+    setShop([
+      {
+        img: s1,
+        name: 'Sneakers #1',
+        price: 256,
+        qty: 1,
+      },
+      {
+        img: s2,
+        name: 'Sneakers #2',
+        price: 728,
+        qty: 1,
+      },
+      {
+        img: s3,
+        name: 'Sneakers #3',
+        price: 376,
+        qty: 1,
+      },
+      {
+        img: s4,
+        name: 'Sneakers #4',
+        price: 643,
+        qty: 1,
+      },
+      {
+        img: s5,
+        name: 'Sneakers #5',
+        price: 234,
+        qty: 1,
+      },
+      {
+        img: s6,
+        name: 'Sneakers #6',
+        price: 865,
+        qty: 1,
+      },
+      {
+        img: s7,
+        name: 'Sneakers #7',
+        price: 967,
+        qty: 1,
+      },
+      {
+        img: s8,
+        name: 'Sneakers #8',
+        price: 120,
+        qty: 1,
+      },
+      {
+        img: s9,
+        name: 'Sneakers #9',
+        price: 324,
+        qty: 1,
+      },
+      {
+        img: s10,
+        name: 'Sneakers #10',
+        price: 460,
+        qty: 1,
+      },
+      {
+        img: s11,
+        name: 'Sneakers #11',
+        price: 521,
+        qty: 1,
+      },
+      {
+        img: s12,
+        name: 'Sneakers #12',
+        price: 743,
+        qty: 1,
+      },
+      {
+        img: s13,
+        name: 'Sneakers #13',
+        price: 853,
+        qty: 1,
+      },
+      {
+        img: s14,
+        name: 'Sneakers #14',
+        price: 634,
+        qty: 1,
+      },
+      {
+        img: s15,
+        name: 'Sneakers #15',
+        price: 548,
+        qty: 1,
+      },
+    ]);
+  }, []);
 
   return (
     <div className="grid gap-x-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 justify-items-center">
-      {items.map((item, index) => (
-        <div key={index} className="mt-4 bg-slate-700 grid grid-cols-2 grid-rows-4 gap-x-5 w-fit h-[228px] p-6">
+      {shop.map((item, index) => (
+        <div
+          key={index}
+          className="mt-4 bg-slate-700 grid grid-cols-2 grid-rows-5 gap-x-5 w-fit h-[228px] p-6 rounded"
+        >
           <img
-            className="col-start-1 row-start-1 row-span-4 "
+            className="col-start-1 row-start-1 row-span-5 "
             width="180px"
             height="180px"
             src={item.img}
             alt=""
           />
-          <div>
-            {item.name} #{index + 1}
-          </div>
+          <div>{item.name}</div>
           <div className="text-green-400">IN STOCK</div>
           <div>${item.price}</div>
+          <div className="flex">
+            <label className="flex-grow">Quantity:</label>
+            <input
+              className="text-black w-10 h-6"
+              type="number"
+              defaultValue={item.qty}
+            />
+            <button onClick={() => {}}>+1</button>
+            <button onClick={() => {}}>-1</button>
+          </div>
           <button className="border-2 border-green-500">Add To Cart</button>
         </div>
       ))}
