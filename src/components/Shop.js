a5668d39e7d894008ec261aaa70f6c16c9b64bfe
Brazy <me@brazy.one>
2022-09-25 01:03:36 +0300

    Add to cart

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -22,90 +22,105 @@ function Shop() {
       name: 'Sneakers #1',
       price: 256,
       qty: 1,
+      cart: false,
     },
     {
       img: s2,
       name: 'Sneakers #2',
       price: 728,
       qty: 1,
+      cart: false,
     },
     {
       img: s3,
       name: 'Sneakers #3',
       price: 376,
       qty: 1,
+      cart: false,
     },
     {
       img: s4,
       name: 'Sneakers #4',
       price: 643,
       qty: 1,
+      cart: false,
     },
     {
       img: s5,
       name: 'Sneakers #5',
       price: 234,
       qty: 1,
+      cart: false,
     },
     {
       img: s6,
       name: 'Sneakers #6',
       price: 865,
       qty: 1,
+      cart: false,
     },
     {
       img: s7,
       name: 'Sneakers #7',
       price: 967,
       qty: 1,
+      cart: false,
     },
     {
       img: s8,
       name: 'Sneakers #8',
       price: 120,
       qty: 1,
+      cart: false,
     },
     {
       img: s9,
       name: 'Sneakers #9',
       price: 324,
       qty: 1,
+      cart: false,
     },
     {
       img: s10,
       name: 'Sneakers #10',
       price: 460,
       qty: 1,
+      cart: false,
     },
     {
       img: s11,
       name: 'Sneakers #11',
       price: 521,
       qty: 1,
+      cart: false,
     },
     {
       img: s12,
       name: 'Sneakers #12',
       price: 743,
       qty: 1,
+      cart: false,
     },
     {
       img: s13,
       name: 'Sneakers #13',
       price: 853,
       qty: 1,
+      cart: false,
     },
     {
       img: s14,
       name: 'Sneakers #14',
       price: 634,
       qty: 1,
+      cart: false,
     },
     {
       img: s15,
       name: 'Sneakers #15',
       price: 548,
       qty: 1,
+      cart: false,
     },
   ];
 
@@ -113,7 +128,7 @@ function Shop() {
 
   useEffect(() => {
     setShop(items);
-  // eslint-disable-next-line
+    // eslint-disable-next-line
   }, []);
 
   return (
@@ -121,10 +136,10 @@ function Shop() {
       {shop.map((item, index) => (
         <div
           key={index}
-          className="mt-4 bg-slate-700 grid grid-cols-2 grid-rows-5 gap-x-5 w-fit h-[228px] p-6 rounded"
+          className="mt-4 bg-zinc-800 grid grid-cols-2 grid-rows-5 gap-x-5 w-fit h-[228px] p-6 rounded"
         >
           <img
-            className="col-start-1 row-start-1 row-span-5 "
+            className="col-start-1 row-start-1 row-span-5"
             width="180px"
             height="180px"
             src={item.img}
@@ -167,7 +182,16 @@ function Shop() {
               -1
             </button>
           </div>
-          <button className="border-2 border-green-500">Add To Cart</button>
+          <button
+            className="border-2 border-green-500"
+            onClick={() => {
+              const array = shop.slice();
+              shop[index].cart = true;
+              setShop(array);
+            }}
+          >
+            Add To Cart
+          </button>
         </div>
       ))}
     </div>
