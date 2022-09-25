b95225d7a995850b3260f6a2b3a62325081c7fd9
Brazy <me@brazy.one>
2022-09-25 04:12:01 +0300

    Split components

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
+import Navbar from './components/Navbar.js';
+import Footer from './components/Footer.js';
+import Items from './components/Items';
+import Home from './components/Home.js';
+import Shop from './components/Shop.js';
+import Cart from './components/Cart.js';
+
+const App = () => {
+  const [shop, setShop] = useState([]);
+
+  useEffect(() => {
+    setShop(Items);
+    // eslint-disable-next-line
+  }, []);
+
+  function LayoutsWithNavbar() {
+    return (
+      <>
+        <Navbar shop={shop} />
+        <Outlet />
+        <Footer />
+      </>
+    );
+  }
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LayoutsWithNavbar />}>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/shop"
+            element={<Shop shop={shop} setShop={setShop} />}
+          />
+          <Route
+            path="/cart"
+            element={<Cart shop={shop} setShop={setShop} />}
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ src/components/Items.js
@@ -0,0 +1,125 @@
+import s1 from './images/s1.jpg';
+import s2 from './images/s2.jpg';
+import s3 from './images/s3.jpg';
+import s4 from './images/s4.jpg';
+import s5 from './images/s5.jpg';
+import s6 from './images/s6.jpg';
+import s7 from './images/s7.jpg';
+import s8 from './images/s8.jpg';
+import s9 from './images/s9.jpg';
+import s10 from './images/s10.jpg';
+import s11 from './images/s11.jpg';
+import s12 from './images/s12.jpg';
+import s13 from './images/s13.jpg';
+import s14 from './images/s14.jpg';
+import s15 from './images/s15.jpg';
+
+const Items = [
+  {
+    img: s1,
+    name: 'Sneakers #1',
+    price: 256,
+    qty: 1,
+    cart: false,
+  },
+  {
+    img: s2,
+    name: 'Sneakers #2',
+    price: 728,
+    qty: 1,
+    cart: false,
+  },
+  {
+    img: s3,
+    name: 'Sneakers #3',
+    price: 376,
+    qty: 1,
+    cart: false,
+  },
+  {
+    img: s4,
+    name: 'Sneakers #4',
+    price: 643,
+    qty: 1,
+    cart: false,
+  },
+  {
+    img: s5,
+    name: 'Sneakers #5',
+    price: 234,
+    qty: 1,
+    cart: false,
+  },
+  {
+    img: s6,
+    name: 'Sneakers #6',
+    price: 865,
+    qty: 1,
+    cart: false,
+  },
+  {
+    img: s7,
+    name: 'Sneakers #7',
+    price: 967,
+    qty: 1,
+    cart: false,
+  },
+  {
+    img: s8,
+    name: 'Sneakers #8',
+    price: 120,
+    qty: 1,
+    cart: false,
+  },
+  {
+    img: s9,
+    name: 'Sneakers #9',
+    price: 324,
+    qty: 1,
+    cart: false,
+  },
+  {
+    img: s10,
+    name: 'Sneakers #10',
+    price: 460,
+    qty: 1,
+    cart: false,
+  },
+  {
+    img: s11,
+    name: 'Sneakers #11',
+    price: 521,
+    qty: 1,
+    cart: false,
+  },
+  {
+    img: s12,
+    name: 'Sneakers #12',
+    price: 743,
+    qty: 1,
+    cart: false,
+  },
+  {
+    img: s13,
+    name: 'Sneakers #13',
+    price: 853,
+    qty: 1,
+    cart: false,
+  },
+  {
+    img: s14,
+    name: 'Sneakers #14',
+    price: 634,
+    qty: 1,
+    cart: false,
+  },
+  {
+    img: s15,
+    name: 'Sneakers #15',
+    price: 548,
+    qty: 1,
+    cart: false,
+  },
+];
+
+export default Items;
